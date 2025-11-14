70c1fa0a00a08945da0855c33b8be78276ca29bf
Eduardo Martins <eduardo.martins@atmsistema.com.br>
2025-11-14 13:29:09 -0300

    fix

--- src/app/invite/invite-link-input.tsx
+++ src/app/invite/invite-link-input.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-<<<<<<< HEAD
 import { Copy, Link } from 'lucide-react'
 import { IconButton } from '../../components/icon-button'
 import { InputField, InputIcon, InputRoot } from '../../components/input'
@@ -14,39 +13,17 @@ export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
     navigator.clipboard.writeText(inviteLink)
   }
 
-=======
-import { IconButton } from '@/components/icon-button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { Copy, Link } from 'lucide-react'
-
-interface InviteLinkInputProps {
-  InviteLink: string
-}
-
-export function InviteLinkInput({ InviteLink }: InviteLinkInputProps) {
-  function copyInviteLink() {
-    navigator.clipboard.writeText(InviteLink)
-  }
->>>>>>> b7023c1 (Add Biome)
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
-<<<<<<< HEAD
 
       <InputField readOnly defaultValue={inviteLink} />
 
-=======
-      <InputField readOnly defaultValue={InviteLink} />
->>>>>>> b7023c1 (Add Biome)
       <IconButton className="-mr-2" onClick={copyInviteLink}>
         <Copy className="size-5" />
       </IconButton>
     </InputRoot>
   )
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> b7023c1 (Add Biome)
