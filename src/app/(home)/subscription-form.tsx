cb1b87eb6e75f89795c5312645c8bd484dfbaba2
Eduardo Martins <eduardo.martins@atmsistema.com.br>
2025-11-27 15:05:53 -0300

    fix

--- src/app/(home)/subscription-form.tsx
+++ src/app/(home)/subscription-form.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { subscribeToEvent } from '@/http/api'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight, Mail, User } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { Button } from '../../components/button'
@@ -15,6 +17,7 @@ const subscriptionSchema = z.object({
 type SubscriptionSchema = z.infer<typeof subscriptionSchema>
 
 export function SubscriptionForm() {
+  const router = useRouter()
   const {
     register,
     handleSubmit,
@@ -23,8 +26,13 @@ export function SubscriptionForm() {
     resolver: zodResolver(subscriptionSchema),
   })
 
-  function onSubscribe(data: SubscriptionSchema) {
-    console.log(data)
+  async function onSubscribe({ name, email }: SubscriptionSchema) {
+    const response = await subscribeToEvent({ name, email })
+    console.log(response)
+
+    const subscriberId = response.subscriberId
+
+    router.push(`/invite/${subscriberId}`)
   }
 
   return (
